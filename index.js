543eac09a215243c926982931c45aefb7f5c88f6
Renan Souza <renantrsouza@gmail.com>
2021-07-02 15:04:23 -0400

    add foto, e print de dados

--- index.js
+++ index.js
@@ -5,8 +5,32 @@ function insert(num){
     } else if(nums.length == 1){
         nums.push(num);
         document.getElementById("numCampo2").innerHTML = nums[1];
-        //document.getElementById("teclado__logo").style.display = "none";
+        mudaDisplay("tela__container", "none");
+        mudaDisplay("tela__candidato", "flex");
+        document.getElementById("tela__candidato").style.justifyContent = "space-between";
+        mostraDado('num1', nums[0]);
+        mostraDado('num2', nums[1]);
+        mostraDado('nome', caramelo.nome);
+        mostraDado('partido',caramelo.partido);
+        
+        
+        mostraDado("candidato__foto", '<img src="'+caramelo.foto+'">');
+        
     }
 }
+function mudaDisplay(id, propriedades){
+    document.getElementById(id).style.display = propriedades;
+}
+function mostraDado(id, inner){
+    document.getElementById(id).innerHTML = inner;
+}
 
+let caramelo = {
+    numero: '01',
+    nome: 'Doguin Caramelo',
+    foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRex2f8n-gcya-IaEBU03fK6XCKNPEIZs31wCRH6P7RA2DQfVaYEFfmAZxsu8C7tAuK-eg&usqp=CAU',
+    partido: 'RUA'
+}
+let candidatos = [caramelo];
+console.log(candidatos);
 let nums = [];
