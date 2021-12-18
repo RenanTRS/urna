9fbfaf850762b273b4b0159ce71bcf60f2c3c5ef
Renan Souza <renantrsouza@gmail.com>
2021-12-18 15:25:14 -0400

    Create tela

---
+++ src/components/Tela.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function Tela(){
+    return(
+        <>
+            <div className='tela'>
+                <h1>Presidente</h1>
+                <div className='inputs'>
+                    <input type="number" />
+                    <input type="number" />
+                </div>
+            </div>
+        </>
+    );
+}
