a6285503f418eef0019e9725b977401f1242ef9c
Renan Souza <renantrsouza@gmail.com>
2021-12-20 16:01:33 -0400

    Create fakeData

---
+++ src/components/fakeData.js
@@ -0,0 +1,54 @@
+export const fakeData =  {
+    '10' : {
+        name: 'Doguinho Caramelo',
+        number: '10',
+        avatar: 'https://i0.statig.com.br/bancodeimagens/2f/ym/i8/2fymi85z5vo5pcl5rsnsr3xgi.jpg',
+        partido: 'RUA'
+    },
+    '28' : {
+        name: 'Julius',
+        number: '28',
+        avatar: 'https://conteudo.imguol.com.br/c/entretenimento/8d/2017/11/09/terry-crews-em-cena-de-todo-mundo-odeia-o-chris-1510241395164_v2_450x450.jpg',
+        partido: 'Se eu não comprar o desconto é maior'
+    },
+    '72' : {
+        name: 'Seu Madruga',
+        number: '72',
+        avatar: 'https://pbs.twimg.com/media/EDf3u4WXkAAbjW5.png',
+        partido: 'A vingança nunca é plena, mata a alma e envenena'
+    },
+    '51' : {
+        name: 'Agostinho Carrara',
+        number: '51',
+        avatar: 'http://s2.glbimg.com/34jyFQ0D7QORvSpxjRK14zAEmjnUqL-UVEuaPrn6SYo-eKUhtAwqByBTGyh2F6TI/i.glbimg.com/og/ig/f/original/2014/06/23/agostinho.jpg',
+        partido: 'Partido Carrara ou Carrara Partido'
+    }
+}
+
+/*
+const doguinhoCaramelo = {
+    name: 'Doguinho Caramelo',
+    number: '10',
+    avatar: 'https://i0.statig.com.br/bancodeimagens/2f/ym/i8/2fymi85z5vo5pcl5rsnsr3xgi.jpg',
+    partido: 'RUA'
+}
+const julius = {
+    name: 'Julius',
+    number: '28',
+    avatar: 'https://conteudo.imguol.com.br/c/entretenimento/8d/2017/11/09/terry-crews-em-cena-de-todo-mundo-odeia-o-chris-1510241395164_v2_450x450.jpg',
+    partido: 'Se eu não comprar o desconto é maior'
+}
+const seuMadruga = {
+    name: 'Seu Madruga',
+    number: '72',
+    avatar: 'https://pbs.twimg.com/media/EDf3u4WXkAAbjW5.png',
+    partido: 'A vingança nunca é plena, mata a alma e envenena'
+}
+const agostinho = {
+    name: 'Agostinho Carrara',
+    number: '51',
+    avatar: 'http://s2.glbimg.com/34jyFQ0D7QORvSpxjRK14zAEmjnUqL-UVEuaPrn6SYo-eKUhtAwqByBTGyh2F6TI/i.glbimg.com/og/ig/f/original/2014/06/23/agostinho.jpg',
+    partido: 'Partido Carrara ou Carrara Partido'
+}*/
+
+//export const fakeData = [doguinhoCaramelo, julius, seuMadruga, agostinho];
