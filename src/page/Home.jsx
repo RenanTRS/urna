fc6660f6740da08405d830044a25cdafabe88085
Renan Souza <renantrsouza@gmail.com>
2021-12-20 13:27:12 -0400

    Create context and state

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {createContext, useState} from 'react';
 
 import { Tela } from '../components/Tela';
 import { Teclado } from '../components/Teclado';
 
 import '../style/style.scss';
 
+export const TesteContext = createContext({});
+
 export function Home(){
+
+    const [numOne, setNumOne] = useState('');
+    const [numTwo, setNumTwo] = useState('');
+    
     return (
         <main>
-            <Tela />
-            <Teclado />
+            <TesteContext.Provider value={{numOne, setNumOne, numTwo, setNumTwo}}>
+                <Tela />
+                <Teclado />
+            </TesteContext.Provider>
         </main>
     );
 }
