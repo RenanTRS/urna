deaf384c8f0680378455de1d1c7fde8423955c6a
Renan Souza <renantrsouza@gmail.com>
2021-12-22 16:42:30 -0400

    Create states

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -12,10 +12,12 @@ export function Home(){
     const [numOne, setNumOne] = useState('');
     const [numTwo, setNumTwo] = useState('');
     const [status, setStatus] = useState(0);
+    const [numero, setNumero] = useState('');
+    const [candidate, setCandidate] = useState({});
     
     return (
         <main>
-            <TesteContext.Provider value={{numOne, setNumOne, numTwo, setNumTwo, status, setStatus}}>
+            <TesteContext.Provider value={{numOne, setNumOne, numTwo, setNumTwo, status, setStatus, numero, setNumero, candidate, setCandidate}}>
                 <Tela />
                 <Teclado />
             </TesteContext.Provider>
