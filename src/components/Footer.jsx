768a0ba9e095f125bb77846289d03a5856cfe4d4
Renan Souza <renantrsouza@gmail.com>
2021-12-22 16:40:34 -0400

    Create component footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Footer(){
+    return(
+        <footer>
+            <span>Aperta a tecla</span>
+            <p>VERDE para CONFIRMAR</p>
+            <p>LARANJA para CORRIGIR</p>
+        </footer>
+    );
+}
