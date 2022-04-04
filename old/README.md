92e2ad721a5cfde7459eef304a913e0620f8e187
Renan Souza <renantrsouza@gmail.com>
2022-04-04 13:10:47 -0400

    rename files

--- old/README.md
+++
@@ -1,5 +0,0 @@
-# Urna
-Uma versão web da urna eletrônica.
-## Tecnologias
-- HTML
-- CSS
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -3,9 +3,6 @@ import React, {createContext, useState} from 'react';
 import { Tela } from '../components/Tela';
 import { Teclado } from '../components/Teclado';
 
-//import '../style/style.scss';
-//import { Style } from '../style/base';
-
 export const TesteContext = createContext({});
 
 export function Home(){
