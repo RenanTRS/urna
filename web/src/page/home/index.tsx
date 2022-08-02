430af7f4194a1544db6761c874b443f1e12c1404
Renan Souza <renantrsouza@gmail.com>
2022-08-02 10:22:37 -0400

    add: base files

---
+++ web/src/page/home/index.tsx
@@ -0,0 +1,13 @@
+import {MainStyled} from './style'
+
+export const Home = () => {
+    return (
+        <MainStyled>
+            <h1>Hellow world</h1>
+            {/*
+            Tela 
+            Teclado
+            */}
+        </MainStyled>
+    )
+}
