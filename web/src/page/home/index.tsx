7844969633034d94c0ea8382eb85acf81b355b09
Renan Souza <renantrsouza@gmail.com>
2022-08-03 17:59:45 -0400

    add: page

--- web/src/page/home/index.tsx
+++ web/src/page/home/index.tsx
@@ -1,13 +1,34 @@
-import {MainStyled} from './style'
+import * as S from './Home.style'
+import { statusType } from 'types/status'
+
+import { Keyboard } from 'components/keyboard'
+import { View } from 'components/view'
+import { useState } from 'react'
+import { CandidateType } from 'types/candidate'
 
 export const Home = () => {
-    return (
-        <MainStyled>
-            <h1>Hellow world</h1>
-            {/*
-            Tela 
-            Teclado
-            */}
-        </MainStyled>
-    )
+  const [number, setNumber] = useState<string>('')
+  const [status, setStatus] = useState<statusType>('Begin')
+  const [candidate, setCandidate] = useState<CandidateType>()
+
+  const handlerNumber = (value: string) => {
+    setNumber(value)
+  }
+
+  const handlerStatus = (value: statusType, candidate?: CandidateType) => {
+    setStatus(value)
+    setCandidate(candidate)
+  }
+
+  return (
+    <S.MainStyled>
+      <View
+        number={number}
+        status={status}
+        handlerStatus={handlerStatus}
+        candidate={candidate!}
+      />
+      <Keyboard handlerNumber={handlerNumber} handlerStatus={handlerStatus} />
+    </S.MainStyled>
+  )
 }
