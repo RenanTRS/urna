1910a5f89f5032d078cd6d769bbdab9bc53992d4
Renan Souza <renantrsouza@gmail.com>
2022-08-03 18:00:24 -0400

    add: footer

---
+++ web/src/components/footer/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './Footer.style'
+
+export const Footer = () => {
+  return (
+    <S.FooterContainer>
+      <span>Aperta a tecla</span>
+      <p>VERDE para CONFIRMAR</p>
+      <p>LARANJA para CORRIGIR</p>
+    </S.FooterContainer>
+  )
+}
