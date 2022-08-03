59ca14d07fd0ae2634b008f82bbc8ca82c02fea0
Renan Souza <renantrsouza@gmail.com>
2022-08-03 18:03:43 -0400

    style: add style to box of numbers

---
+++ web/src/components/numbers/index.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Numbers = styled.span`
+  border: 2px solid black;
+
+  padding: 0.5rem 1rem;
+  &:not(:last-child) {
+    margin-right: 1rem;
+  }
+`
