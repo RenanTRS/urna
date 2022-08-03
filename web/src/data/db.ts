7c6b94eb7cc3c49b5bfff0204a725fc4ec8277d0
Renan Souza <renantrsouza@gmail.com>
2022-08-03 18:03:58 -0400

    add: database to candidates

---
+++ web/src/data/db.ts
@@ -0,0 +1,31 @@
+import DogImg from 'assets/images/candidates/dog.png'
+import JuliusImg from 'assets/images/candidates/julius.png'
+import MadrugaImg from 'assets/images/candidates/madruga.png'
+import AgostinhoImg from 'assets/images/candidates/agostinho.png'
+
+export const candidates = [
+  {
+    name: 'Doguinho Caramelo',
+    number: '10',
+    avatar: DogImg,
+    politicalParty: 'RUA'
+  },
+  {
+    name: 'Julius',
+    number: '28',
+    avatar: JuliusImg,
+    politicalParty: 'Desconto'
+  },
+  {
+    name: 'Agostinho Carrara',
+    number: '51',
+    avatar: AgostinhoImg,
+    politicalParty: 'Partido Carrara'
+  },
+  {
+    name: 'Seu Madruga',
+    number: '72',
+    avatar: MadrugaImg,
+    politicalParty: 'Chaves'
+  }
+]
